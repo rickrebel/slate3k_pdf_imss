7c4dd0709b48bcc627365811cd65ba14541c20b5
David Sanginés <dvdsansay@gmail.com>
2024-06-20 00:39:12 -0600

    Add main

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logger.info(f'Start...')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
